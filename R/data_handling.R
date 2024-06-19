bf56b1207b585668de3c532ad86616d3b0b66b76
xeniamuenger <xenia.muenger@gmail.com>
2024-06-20 09:40:23 +1000

    init commit - collate_one_gen and collate_gen
    two new functions to load .gen files into R

--- R/data_handling.R
+++ R/data_handling.R
@@ -622,3 +622,142 @@ lookup_table <- function(data,
     if (save2disk) df2disk(LookUpT, dir_out, fname, "_LookUpT")
     return(LookUpT)
 }
+
+
+
+#' Collate one local Vortex genetics output file into a data.frame
+#'
+#' \code{collate_one_gen} parses one Vortex .gen and returns the
+#' data within as one data.frame.
+#'
+#' @param filename The fully qualified filename of a Vortex .dat file
+#' @param runs The number of simulation runs
+#' @param populations The number of populations in the simulation 
+#' @param verbose Progress messages, default: FALSE
+#' @return A data.frame with data from one .gen 
+#' require(vortexRdata)
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																											
+
+collate_one_gen <- function(filename,
+                            runs, 
+                            populations,
+                            verbose = FALSE){
+		lines <- readLines(filename)  ## need to adapt this to read multiple files (I think)
+
+  
+  # Blocks of population data start with 'Population' with more than one population, a
+  # last block 'Metapopulation' exists, but is ignored
+  locLn <- grep(pattern = "^\\$   Locus", lines)
+  locN <- length(locLn)
+  
+  # Locus vector
+  loc <- c(2:locN) # skipping locus 1 which is the default and more than 2 alleles
+  
+  # create data frame of only biallelic loci
+  gen_data <- as.data.frame(matrix(NA, nrow=0, ncol=6))
+  colnames(gen_data) <- c("Population", "Locus", "meanFrequency_A1", "meanFrequency_A2", 
+                          "probPers_A1", "probPers_A2")
+  
+  # looping over each locus and extracting the information to build the new data.frame
+  pop <- populations
+  popMeta <- 1
+  for (i in  loc) {
+    print(i)
+    # Locus Number
+    Locus <- i
+    
+    if(pop != 1) {
+      popMeta <- pop + 1 # adding meta population to the count
+    }
+    for (p in 1:popMeta) {
+      print(p)
+      # mean allele frequencies
+      meanFreq <- unlist(strsplit(lines[(3 + 2*popMeta +3 + (i-2)*(2*(popMeta+1)) + p*2)], split = ";"))  # (3 [header] + 2*popMeta [skip locus 1] + 3 [identify line of interest]] + (i-2)*(2*(popMeta+1)) [skip previous loci] + p*2 [skip previous populations])
+      # probability of persistence
+      probPers <- unlist(strsplit(lines[(3 + 2*popMeta +4 + (i-2)*(2*(popMeta+1)) + p*2)], split = ";"))  # (3 [header] + 2*popMeta [skip locus 1] + 3 [identify line of interest] + (i-2)*(2*(popMeta+1)) [skip previous loci] + p*2 [skip previous populations])
+      # putting it all together
+      gen_data[nrow(gen_data) + 1,] = c(meanFreq[1], Locus, meanFreq[2],
+                                        meanFreq[3], probPers[2], probPers[3])
+    }
+  }
+  gen_data$meanFrequency_A1 <- as.numeric(gen_data$meanFrequency_A1)
+  gen_data$meanFrequency_A2  <- as.numeric(gen_data$meanFrequency_A2)
+  gen_data$probPers_A1 <- as.numeric(gen_data$probPers_A1)
+  gen_data$probPers_A2 <- as.numeric(gen_data$probPers_A2)
+  return(gen_data)
+}
+
+
+
+
+#' Collate Vortex .gen output files into one data.frame
+#'
+#' \code{collate_gen} collates all Vortex output files matching a given project
+#' name (and scenario name when relevant) in a given directory into one
+#' data.frame using \code{collate_one_gen}.
+#'
+#' The number of Vortex simulation runs has to be specified, as it cannot be
+#' inferred by vortexR.
+#'
+#' \code{dir_in} may contain other files; only files matching the project (and,
+#' optionally, the scenario) name will be read.
+#'
+#' \code{dir_out} is created within the working directory unless a full path is
+#' provided.
+#'
+#' If no matching files are found in the given directory, an error is reported.
+#'
+#' When \code{verbose=TRUE} the progress (i.e. the file being read) is reported
+#' on screen.
+#'
+#' @param project The Vortex project name to be imported
+#' @param populations The number of populations in the simulation
+#' @param scenario The scenario name if ST, default: NULL
+#' @param dir_in The local folder containing Vortex files, default: NULL. If
+#'   not specified, will fall back to use current working directory.
+#' @param save2disk Whether to save the data as rda and csv, default: TRUE
+#' @param dir_out The local path to store the output. Default: ProcessedData
+#' @param verbose Progress messages, default: TRUE
+#' @inheritParams collate_one_gen
+#' @return a data.frame with data from all matching Vortex files or NULL
+#' @import vortexRdata
+
+ollate_gen <- function(project,
+                       populations, 
+                       scenario = NULL, 
+                       dir_in = NULL,
+					   runs = NULL, 
+                       #dec_sep = ".",
+                       save2disk = TRUE,
+                       dir_out = "ProcessedData", 
+                       verbose = TRUE) {
+  if (is.null(scenario)) {
+    fname <- project
+    pat <- paste0(fname, ".gen")
+  } else {
+    fname <- paste(project, scenario, sep = "_")
+    pat <- paste0("^", fname, ".*\\.gen$")
+  }
+  
+  if (is.null(dir_in)) dir_in <- getwd()
+  setwd(dir_in)
+    
+  files <- get_file_paths(path = dir_in,
+                          pattern = pat,
+                          fn_name = "collate_gen",
+                          fname = fname,
+                          verbose = verbose)
+  
+  d <- data.frame()
+  if (verbose) message("vortexR::collate_gen is parsing:")
+  scen <- 0
+  for (filename in files) {
+    if (verbose) message(filename, "\r")
+    scen <- scen + 1
+    one_gen <- collate_one_gen(filename, runs, populations)
+    one_gen$scenario <- scen
+    d <- rbind(d, one_gen)
+  }
+  if (save2disk) df2disk(d, dir_out, fname, "_gendata")
+  return(d)
+}
